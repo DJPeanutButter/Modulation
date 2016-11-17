3f9e71daaa351c5fd01993adf0db69cf9f02f4c8
Jonny Lawless <JonnyLawless78@gmail.com>
2016-11-17 15:09:13 -0600

    Create Background.h

---
+++ Background.h
@@ -0,0 +1,9 @@
+#ifndef _BACKGROUND_H_
+#define _BACKGROUND_H_
+
+#include "Color.h"
+
+void	DrawBackground	(Color c1, Color c2);
+void	DrawBackground	(float xMin, float xMax, float yMin, float yMax, Color c1, Color c2);
+
+#endif
