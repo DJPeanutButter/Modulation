04bdd51372f21b2d0aa36ab70cc8ea04ece84dea
Jonny Lawless <JonnyLawless78@gmail.com>
2016-11-17 15:21:32 -0600

    Update Color.h

--- Color.h
+++ Color.h
@@ -17,8 +17,8 @@ struct Color{
 	Color (float r, float g, float b);
 };
 
-Color		COLOR		(float r, float g, float b);
-Color		INVERT		(Color &c);
-void inline	SetColor	(Color c);
+Color	COLOR		(float r, float g, float b);
+Color	INVERT		(Color &c);
+void	SetColor	(Color c);
 
 #endif
