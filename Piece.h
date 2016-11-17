e41091d394e8e97937bc1188606822bd4ad6ca77
Jonny Lawless <JonnyLawless78@gmail.com>
2016-11-17 14:34:52 -0600

    Update Piece.h

--- Piece.h
+++ Piece.h
@@ -3,7 +3,20 @@
 
 #include "Color.h"
 
-Struct Piece{
+#define PT_CIRCLE	0
+#define PT_FAN		1
+#define	PT_SELECT	2
+#define PT_OPTION	3
+#define PT_KNIGHT	4
+#define PT_KING		5
+#define PT_BISHOP	6
+#define PT_ROOK		7
+#define PT_QUEEN	8
+
+
+#define PI	3.1415926
+
+struct Piece{
 	bool	alive;
 	char	row, col, type, dir;
 	Color	color;
@@ -19,4 +32,4 @@ Struct Piece{
 	Piece (char r, char c, char t);
 };
 
-#endif _PIECE_H_
+#endif
